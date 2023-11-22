951349b5750d246adefca84baa738c9029cf0a3b
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-11-22 17:43:28 +0800

    fixed missing dbConnect

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,7 +1,9 @@
 import { SITE } from "@/config/config";
+import dbConnect from "@/db/database";
 import Post from "@/models/Post";
 
 export default async function sitemap() {
+  await dbConnect();
   let posts = await Post.find();
 
   let blogPosts = posts.map((post) => ({
