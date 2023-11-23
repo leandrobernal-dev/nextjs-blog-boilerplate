3503b2a155b19b8e2982d87669003198bfd19b22
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-11-23 13:42:22 +0800

    added loading component to NoSSR

--- src/utils/NoSSR.js
+++ src/utils/NoSSR.js
@@ -5,4 +5,9 @@ const NoSSR = ({ children }) => <>{children}</>;
 
 export default dynamic(() => Promise.resolve(NoSSR), {
   ssr: false,
+  loading: () => (
+    <p className="my-10 flex h-36 w-full animate-pulse items-center justify-center rounded-lg bg-zinc-800">
+      Loading...
+    </p>
+  ),
 });
