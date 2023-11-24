ade02b8b92f8ee6a7cf135fb7d13b2e05d719e8d
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-11-25 00:46:57 +0800

    homepage intro update

--- src/app/page.js
+++ src/app/page.js
@@ -8,27 +8,23 @@ import Posts from "@/components/Posts";
 export default function Home() {
   return (
     <>
-      <section className="flex flex-col gap-4 py-2">
-        <h1 className="text-4xl font-bold">Lorem Ipsum</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex eveniet
-          libero fuga dolores, deserunt optio modi, exercitationem veniam
-          ducimus voluptas reprehenderit quod fugit laborum reiciendis? Eius
-          consequatur impedit dicta facilis molestias, maxime at, eligendi
-          deleniti temporibus dolore veritatis, nostrum recusandae dignissimos
-          similique eos minima perferendis eveniet. Dolor, distinctio nulla
-          expedita eligendi aut, atque similique sint sit beatae, praesentium
-          corporis voluptatibus.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, sint.
+      <section className="flex flex-col gap-2 py-2">
+        <h1 className="text-xl font-bold">hey, it's lei 👋</h1>
+        <p className="text-sm">
+          I'm a full stack wizard weaving digital magic! From crafting slick
+          interfaces to building rock-solid functionality, I've got you covered.
+          When I'm not coding, you might find me tinkering with robots or diving
+          into epic anime worlds for inspiration. My passion for robotics fuels
+          my creativity, while my love for anime infuses a unique flair into my
+          coding adventures. Join me on this journey through tech and
+          imagination!
         </p>
 
         <div>
           {SOCIALS.length > 0 ? (
             <p>
-              <span>Social Links:</span>
-              <span className="flex gap-2 hover:text-orange-500">
+              <span className="">Social Links:</span>
+              <span className="flex justify-start gap-2 ">
                 <SocialLinks />
               </span>
             </p>
@@ -38,7 +34,7 @@ export default function Home() {
         </div>
       </section>
 
-      <hr className="my-8 " />
+      <hr className="my-4" />
 
       <section className="py-2">
         <h2 className="pb-6 text-3xl">Featured</h2>
