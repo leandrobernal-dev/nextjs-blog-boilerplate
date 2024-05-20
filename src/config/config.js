de7cd45d37bc1669743c6e0ae794478c9aa283b3
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-05-20 15:29:14 +0800

    Updated config

--- src/config/config.js
+++ src/config/config.js
@@ -4,15 +4,15 @@ export const SITE = {
   website: "https://blog.leiisme.com/",
   author: "Leandro Bernal",
   description: "Software Development and Tech Blogs",
-  title: "leiisme",
-  logo: `<lei/>`,
+  title: "Leandro Bernal",
+  logo: `Leandro Bernal`,
 };
 
 export const SOCIALS = [
-  // {
-  //   name: "Github",
-  //   href: "https://github.com/leandrobernal-dev",
-  //   linkTitle: `${SITE.title} on Github`,
-  //   icon: <Github />,
-  // },
+  {
+    name: "Github",
+    href: "https://github.com/leandrobernal-dev",
+    linkTitle: `${SITE.title} on Github`,
+    icon: <Github />,
+  },
 ];
