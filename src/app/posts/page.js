4e74453afda5b60ac67320d668af04cd4f243874
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-05-20 15:29:32 +0800

    Updated Posts Page

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,15 +1,10 @@
 import { PostSkeleton } from "@/components/PostSkeleton";
 import Posts from "@/components/Posts";
-import { headers } from "next/headers";
 import { Suspense } from "react";
 
-export default function PostsPage(req) {
-  const headerList = headers();
-  const ip = headerList.get("x-forwarded-for");
+export default function PostsPage() {
   return (
     <div>
-      <h1 className="pb-4 text-3xl font-bold">Posts</h1>
-
       <Suspense fallback={<PostSkeleton />}>
         <Posts category={"all"} />
       </Suspense>
