9a7c8e133a9eb3ac2eacea16983990d580aa6b27
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-05-20 17:01:30 +0800

    Updated Posts component and added getPosts functions

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,81 +1,121 @@
 import { unstable_noStore as noStore } from "next/cache";
 
-import getPosts from "@/utils/getPosts";
 import Link from "next/link";
 import HTMLReactParser from "html-react-parser";
+import getPost, { getPosts, getTotalPostsCount } from "@/utils/getPosts";
+import { SITE } from "@/config/config";
 
-const PostElements = ({ posts }) => {
+const PostElements = ({ posts, paginate, totalPostCount, page }) => {
+  const totalPages = Math.ceil(totalPostCount / SITE.postPerPage);
   return (
-    <ul className="divide-y divide-gray-200 dark:divide-gray-700">
-      {posts.map((post) => (
-        <li className="py-12">
-          <article>
-            <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-              <dl>
-                <dt className="sr-only">Published on</dt>
-                <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                  <time dateTime={post.date}>
-                    {new Date(post.date).toLocaleDateString("en-us", {
-                      month: "long",
-                      day: "2-digit",
-                      year: "numeric",
-                    })}
-                  </time>
-                </dd>
-              </dl>
-              <div className="space-y-5 xl:col-span-3">
-                <div className="space-y-6">
-                  <div>
-                    <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                      <Link
-                        className="text-gray-900 dark:text-gray-100"
-                        href="/blog/release-of-tailwind-nextjs-starter-blog-v2.0"
-                      >
-                        {post.title}
-                      </Link>
-                    </h2>
-                    <div className="flex flex-wrap">
-                      {post.tags.nodes.map((tag) => (
+    <div>
+      <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+        {posts.map((post) => (
+          <li className="py-12" key={post.id}>
+            <article>
+              <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                <dl>
+                  <dt className="sr-only">Published on</dt>
+                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                    <time dateTime={post.date}>
+                      {new Date(post.date).toLocaleDateString("en-us", {
+                        month: "long",
+                        day: "2-digit",
+                        year: "numeric",
+                      })}
+                    </time>
+                  </dd>
+                </dl>
+                <div className="space-y-5 xl:col-span-3">
+                  <div className="space-y-6">
+                    <div>
+                      <h2 className="text-2xl font-bold leading-8 tracking-tight">
                         <Link
-                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400 mr-3 text-sm font-medium uppercase"
-                          href={"/tags/" + tag.name}
-                          key={tag.id}
+                          className="text-gray-900 dark:text-gray-100"
+                          href="/blog/release-of-tailwind-nextjs-starter-blog-v2.0"
                         >
-                          {tag.name}
+                          {post.title}
                         </Link>
-                      ))}
+                      </h2>
+                      <div className="flex flex-wrap">
+                        {post.tags.nodes.map((tag) => (
+                          <Link
+                            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400 mr-3 text-sm font-medium uppercase"
+                            href={"/tags/" + tag.name}
+                            key={tag.id}
+                          >
+                            {tag.name}
+                          </Link>
+                        ))}
+                      </div>
+                    </div>
+                    <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                      <div>{HTMLReactParser(post.excerpt)}</div>
                     </div>
                   </div>
-                  <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                    <div>{HTMLReactParser(post.excerpt)}</div>
+                  <div className="text-base font-medium leading-6">
+                    <Link
+                      className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                      aria-label={post.title}
+                      href={"/posts/" + post.slug}
+                    >
+                      Read more →
+                    </Link>
                   </div>
                 </div>
-                <div className="text-base font-medium leading-6">
-                  <Link
-                    className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                    aria-label={post.title}
-                    href={"/posts/" + post.slug}
-                  >
-                    Read more →
-                  </Link>
-                </div>
               </div>
-            </div>
-          </article>
-        </li>
-      ))}
-    </ul>
+            </article>
+          </li>
+        ))}
+      </ul>
+      {paginate && (
+        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+          <nav className="flex justify-between">
+            <Link
+              rel="next"
+              href={`/posts/page/${Number(page) - 1}`}
+              className={
+                page - 1 === 0
+                  ? "pointer-events-none select-none text-gray-500"
+                  : ""
+              }
+            >
+              Previous
+            </Link>
+            <span>
+              {page}{" "}
+              {totalPostCount > SITE.postPerPage ? "of " + totalPages : ""}
+            </span>
+            <Link
+              rel="next"
+              href={`/posts/page/${Number(page) + 1}`}
+              className={
+                page >= totalPages
+                  ? "pointer-events-none select-none text-gray-500"
+                  : ""
+              }
+            >
+              Next
+            </Link>
+          </nav>
+        </div>
+      )}
+    </div>
   );
 };
 
-export default async function Posts({ category, searchQuery, page }) {
+export default async function Posts({
+  searchQuery,
+  page = 1,
+  paginate = false,
+}) {
   noStore();
 
   // await new Promise((resolve) => setTimeout(resolve, 30000)); // Simulate slow data fetching
 
   let posts;
   let query;
-
+  // Return posts for search
   if (searchQuery) {
     query = {
       query: `
@@ -92,7 +132,7 @@ export default async function Posts({ category, searchQuery, page }) {
             }
           `,
     };
-    let result = await getPosts(query);
+    let result = await getPost(query);
     posts = result.posts.nodes;
 
     return (
@@ -106,31 +146,16 @@ export default async function Posts({ category, searchQuery, page }) {
     );
   }
 
-  query = {
-    query: `
-          {
-            posts(first: 5) {
-              nodes {
-                id
-                slug
-                title
-                date
-                excerpt
-                tags {
-                  nodes {
-                    id
-                    name
-                  }
-                }
-              }
-            }
-          }
-    
-        `,
-  };
+  posts = await getPosts(page);
+  const totalPostCount = await getTotalPostsCount();
+  console.log(totalPostCount);
 
-  let result = await getPosts(query);
-  posts = result.posts.nodes;
-
-  return <PostElements posts={posts} />;
+  return (
+    <PostElements
+      posts={posts}
+      paginate={paginate}
+      totalPostCount={totalPostCount}
+      page={page}
+    />
+  );
 }
--- src/utils/getPosts.js
+++ src/utils/getPosts.js
@@ -1,3 +1,4 @@
+import { SITE } from "@/config/config";
 import axios from "axios";
 
 export const headers = {
@@ -6,11 +7,117 @@ export const headers = {
   },
 };
 
-export default async function getPosts(query) {
+export async function getPosts(page = 1) {
+  let afterCursor = null;
+
+  for (let i = 1; i < page; i++) {
+    const query = {
+      query: `
+        query GetPosts($first: Int!, $after: String) {
+          posts(first: $first, after: $after) {
+            pageInfo {
+              endCursor
+              hasNextPage
+            }
+            nodes {
+              id
+              slug
+              title
+              date
+              excerpt
+              tags {
+                nodes {
+                  id
+                  name
+                }
+              }
+            }
+          }
+        }
+      `,
+      variables: {
+        first: SITE.postPerPage,
+        after: afterCursor,
+      },
+    };
+
+    const result = await axios
+      .post(process.env.GRAPHQL_URI, query, headers)
+      .then(({ data }) => {
+        return data.data.posts;
+      });
+
+    if (!result.pageInfo.hasNextPage) {
+      break;
+    }
+
+    afterCursor = result.pageInfo.endCursor;
+  }
+
+  const query = {
+    query: `
+      query GetPosts($first: Int!, $after: String) {
+        posts(first: $first, after: $after) {
+          pageInfo {
+            endCursor
+            hasNextPage
+          }
+          nodes {
+            id
+            slug
+            title
+            date
+            excerpt
+            tags {
+              nodes {
+                id
+                name
+              }
+            }
+          }
+        }
+      }
+    `,
+    variables: {
+      first: SITE.postPerPage,
+      after: afterCursor,
+    },
+  };
+
+  const finalResult = await axios
+    .post(process.env.GRAPHQL_URI, query, headers)
+    .then(({ data }) => {
+      return data.data.posts;
+    });
+
+  return finalResult.nodes;
+}
+
+export default async function getPost(query) {
   const posts = await axios
     .post(process.env.GRAPHQL_URI, query, headers)
     .then(({ data }) => {
       return data.data;
     });
   return posts;
+}
+
+export async function getTotalPostsCount() {
+  const query = {
+    query: `
+      query GetPostsInfo {
+        posts {
+          nodes {
+            id
+          }
+        }
+      }
+    `,
+  };
+
+  const result = await axios
+    .post(process.env.GRAPHQL_URI, query, headers)
+    .then(({ data }) => data.data.posts);
+
+  return result.nodes.length;
 }
