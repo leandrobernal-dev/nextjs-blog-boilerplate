3432da2fca90c164023850fbbf9e9cee8883491d
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-05-21 18:37:15 +0800

    Updated a tags with Link tags

--- src/app/tags/[tag]/layout.js
+++ src/app/tags/[tag]/layout.js
@@ -9,12 +9,12 @@ export default async function TagPage({ params, children }) {
       <div className="flex sm:space-x-24">
         <div className="hidden h-full max-h-screen min-w-[280px] max-w-[280px] flex-wrap overflow-auto rounded bg-gray-50 pt-5 shadow-md dark:bg-gray-900/70 dark:shadow-gray-800/40 sm:flex">
           <div className="px-6 py-4">
-            <a
+            <Link
               className="font-bold uppercase text-gray-700 hover:text-primary-500 dark:text-gray-300 dark:hover:text-primary-500"
               href="/posts"
             >
               All Posts
-            </a>
+            </Link>
             <ul>
               {tags.map((tag) => {
                 return tag.slug === params.tag ? (
