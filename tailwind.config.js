3f9bb2c5cc7a487e2b0249e44f956defd2a6d423
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-05-21 10:58:59 +0800

    Updated tailwind config for shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,29 @@ module.exports = {
   darkMode: "class",
   theme: {
     extend: {
+      container: {
+        center: true,
+        padding: "2rem",
+        screens: {
+          "2xl": "1400px",
+        },
+      },
+      extend: {
+        keyframes: {
+          "accordion-down": {
+            from: { height: "0" },
+            to: { height: "var(--radix-accordion-content-height)" },
+          },
+          "accordion-up": {
+            from: { height: "var(--radix-accordion-content-height)" },
+            to: { height: "0" },
+          },
+        },
+        animation: {
+          "accordion-down": "accordion-down 0.2s ease-out",
+          "accordion-up": "accordion-up 0.2s ease-out",
+        },
+      },
       lineHeight: {
         11: "2.75rem",
         12: "3rem",
