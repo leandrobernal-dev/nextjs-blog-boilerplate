6358e077f4f98657d232a7d61997712d035f7531
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-05-21 17:05:00 +0800

    Article heading color set to primary

--- src/components/Article.js
+++ src/components/Article.js
@@ -4,7 +4,7 @@ import HTMLReactParser from "html-react-parser";
 
 export default function Article({ content }) {
   return (
-    <article className="prose dark:prose-invert prose-img:w-full prose-img:max-w-2xl">
+    <article className="prose prose-gray dark:prose-invert prose-img:w-full prose-img:max-w-2xl dark:prose-headings:text-primary-500">
       {HTMLReactParser(content)}
     </article>
   );
