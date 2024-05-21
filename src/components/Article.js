39fbca7130d6cc85222c903bdf1ffdda7f46699c
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-05-21 16:07:24 +0800

    Updated blog post img size

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,5 +3,9 @@
 import HTMLReactParser from "html-react-parser";
 
 export default function Article({ content }) {
-  return <article>{HTMLReactParser(content)}</article>;
+  return (
+    <article className="sm:prose-img:w-[80%] md:prose-img:w-[70%] lg:prose-img:w-1/2">
+      {HTMLReactParser(content)}
+    </article>
+  );
 }
