46691a425ddc3f049a1632c8257659420c6c550f
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-05-21 10:56:18 +0800

    Updated context provider

--- src/context/UserDataContext.js
+++ src/context/UserDataContext.js
@@ -5,19 +5,10 @@ import { createContext, useState } from "react";
 export const UserDataContext = createContext();
 
 export default function ContextProvider({ children }) {
-  const [blogs, setBlogs] = useState([
-    {
-      title: "Astro 1.0",
-      createdAt: "Novermber 22, 2023 | 01:26 PM",
-      slug: "astro-v-1",
-      description:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam facere consequatur, quod impedit ullam quisquam!",
-      tags: "astro",
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
 
   return (
-    <UserDataContext.Provider value={{ blogs, setBlogs }}>
+    <UserDataContext.Provider value={{ posts, setPosts }}>
       {children}
     </UserDataContext.Provider>
   );
