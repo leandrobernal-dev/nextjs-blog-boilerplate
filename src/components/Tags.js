35dbeb84d6c5f308e7a9e46e18dee29b76836034
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-05-21 12:57:39 +0800

    Updated Tags component

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -1,12 +1,18 @@
+import Link from "next/link";
+
 export default function Tags({ tags }) {
   return (
-    <section className="my-4 w-full ">
-      <span>Tags:</span>
+    <section className="my-4 flex w-full items-center gap-1">
+      <h4 className="font-medium">Tags:</h4>
       <div className="flex gap-1 text-orange-500">
         {tags.map((tag) => (
-          <span className="rounded-md bg-slate-800 p-2" key={"tags-" + tag.id}>
+          <Link
+            className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+            href={"/tags/" + tag.slug}
+            key={tag.id}
+          >
             {tag.name}
-          </span>
+          </Link>
         ))}
       </div>
     </section>
