8ed0f4f7ff648b90b65615c6b61dcc95be3acf7b
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-05-21 18:54:36 +0800

    Fixed missing key props

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -20,13 +20,13 @@ export default async function PostPage({ params }) {
             <ul>
               {tags.map((tag) => {
                 return tag.slug === params.tag ? (
-                  <li className="my-3">
+                  <li className="my-3" key={tag.id}>
                     <h3 className="inline px-3 py-2 text-sm font-bold uppercase text-primary-500">
                       {`${tag.slug}(${tag.count})`}
                     </h3>
                   </li>
                 ) : (
-                  <li className="my-3">
+                  <li className="my-3" key={tag.id}>
                     <Link
                       className="px-3 py-2 text-sm font-medium uppercase text-gray-500 hover:text-primary-500 dark:text-gray-300 dark:hover:text-primary-500"
                       aria-label="View posts tagged guide"
