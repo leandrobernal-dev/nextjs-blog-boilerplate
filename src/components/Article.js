5890bfa89d6834b8f8f1acd6d5558203572555e9
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-05-23 17:30:59 +0800

    Added shadow on images in posts page

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,7 +2,7 @@ import HTMLReactParser from "html-react-parser";
 
 export default function Article({ content }) {
   return (
-    <div className="prose prose-gray max-w-none pb-8 pt-10 dark:prose-invert prose-img:w-full  dark:prose-headings:text-primary-500">
+    <div className="prose prose-gray max-w-none pb-8 pt-10 dark:prose-invert prose-img:w-full prose-img:shadow-2xl prose-img:shadow-white/20  dark:prose-headings:text-primary-500">
       {HTMLReactParser(content)}
     </div>
   );
