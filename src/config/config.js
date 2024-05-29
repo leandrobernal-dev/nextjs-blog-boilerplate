9d14325120c1fce7d2898d9b3a3a2665a2987cca
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-05-29 20:51:04 +0800

    Updated logo and favicon

--- src/config/config.js
+++ src/config/config.js
@@ -5,8 +5,8 @@ export const SITE = {
   title: "Leandro Bernal",
   logo: (
     <div className="flex items-center gap-1">
-      <img src="/logo.svg" alt="" className="w-16" draggable={false} />
-      <div className="logo-text-container absolute left-8 w-80 overflow-hidden whitespace-nowrap">
+      <img src="/logo_2.svg" alt="" className="w-16" draggable={false} />
+      <div className="logo-text-container absolute left-9 w-80 overflow-hidden whitespace-nowrap">
         <span className="logo-text relative inline-block whitespace-nowrap">
           Leandro Bernal<span className="text-accent">.</span>
         </span>
