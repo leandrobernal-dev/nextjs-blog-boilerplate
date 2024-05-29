8579aeb926fb376cb5107ccc8cff9443caccb8a2
Leandro Bernal <bernalleandro.dev@gmail.com>
2024-05-29 10:29:15 +0800

    Temporarily remove landing page hero text

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-5xl font-bold">Latest</h1>
-          <p className="text-lg text-gray-400">
+          {/* <p className="text-lg text-gray-400">
             I'm a full stack wizard weaving digital magic! From crafting slick
             interfaces to building rock-solid functionality, I've got you
             covered. When I'm not coding, you might find me tinkering with
@@ -17,7 +17,7 @@ export default function Home() {
             for robotics fuels my creativity, while my love for anime infuses a
             unique flair into my coding adventures. Join me on this journey
             through tech and imagination!
-          </p>
+          </p> */}
         </div>
 
         <section className="py-2">
